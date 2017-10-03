f7085cacc4d4a1be4fe54f32696271b2083276dd
Dan Daly <dan.john.daly@gmail.com>
2017-10-03 19:06:38 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,39 @@
+function getFirstSelector( selector ) {
+  return document.querySelector( selector )
+}
+
+function nestedTarget() {
+  return document.querySelector( "#nested .target" )
+}
+
+function increaseRankBy( n ) {
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+
+  for (let i = 0; i < lis.length; i++) {
+    let x = parseInt(lis[i].innerHTML, 10)
+    lis[i].innerHTML = x+n
+  }
+}
+
+function deepestChild() {
+  const grand = document.getElementById("grand-node")
+  var nodes = grand.childNodes
+  var deeper_nodes = []
+
+  do {
+    for( let i in nodes ) {
+      if( nodes[i].hasChildNodes() ) {
+        deeper_nodes.push(nodes[i])
+      }
+    }
+
+    if( deeper_nodes.length > 0 ) {
+      nodes = deeper_nodes.slice(0)
+      deeper_nodes = []
+    }
+    else { return nodes[0] }
+
+  } while( false )
+}
+
+deepestChild()
